9210f4e9c58e0af789282a61af988bbb937e48c9
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-04-25 03:58:26 +0800

    port 8080

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0" , () => {
     console.log(`🚀 伺服器運行在 http://localhost:${PORT}`);
 });
--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,7 @@
 // frontend/src/service/api.js
 import axios from "axios";
 
-// VITE_API_URL 在開發時會是  http://localhost:5000  
+// VITE_API_URL 在開發時會是  http://localhost:8080  
 // 在生產時我們留空，它就變成 "" → 走同一個 domain + /api
 const BASE = import.meta.env.VITE_API_URL || "";
 
