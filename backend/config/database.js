013672e78d45f8e7c8e1737e9d46b4c07388feb6
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-04-24 18:04:31 +0800

    更改環境變數

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,23 @@
 require('dotenv').config();                // ← 載入 .env
 const { Sequelize } = require('sequelize');
 
-const sqlize = new Sequelize(
-  process.env.DB_NAME,                     // 資料庫名稱
-  process.env.DB_USER,                     // 使用者
-  process.env.DB_PASS,                     // 密碼
-  {
-    host: process.env.DB_HOST,             // 主機（localhost 或 Railway Host）
-    port: process.env.DB_PORT,             // 埠號
-    dialect: process.env.DB_DIALECT,       // mysql
-    logging: false,
-    dialectOptions: { charset: 'utf8mb4' } // optional
-  }
-);
+// const sqlize = new Sequelize(
+//   process.env.DB_NAME,                     // 資料庫名稱
+//   process.env.DB_USER,                     // 使用者
+//   process.env.DB_PASS,                     // 密碼
+//   {
+//     host: process.env.DB_HOST,             // 主機（localhost 或 Railway Host）
+//     port: process.env.DB_PORT,             // 埠號
+//     dialect: 'mysql',       // mysql
+//     logging: false,
+//     dialectOptions: { charset: 'utf8mb4' } // optional
+//   }
+// );
+
+const sqlize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: 'mysql',
+  logging: false,
+  dialectOptions: { charset: 'utf8mb4' }
+});
 
 module.exports = sqlize;
