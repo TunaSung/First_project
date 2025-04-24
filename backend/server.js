efe62de711bb65794f1f2571db8cc1bca6b395e0
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-04-24 20:20:51 +0800

    更改一堆路徑啦

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const sqlize = require("./config/database");
 const authRoutes = require("./routes/authRoutes");
