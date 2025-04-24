2eafc48b50ae9e2452d59a38d1874e98650dcb80
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-04-24 18:49:29 +0800

    更改一堆圖片路徑

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -2,6 +2,9 @@ import SignInForm from "../components/Feature/SignInForm"
 import { useState } from "react"
 import { motion, AnimatePresence } from "framer-motion";
 import Register from "../components/Feature/Register"
+import winter from '../assets/images/winter.jpg'
+import winter2 from '../assets/images/winter2.jpg'
+import kami from '../assets/images/kami.jpg'
 
 function SignIn(){
 
@@ -13,13 +16,13 @@ function SignIn(){
     
     return (
         <div id="sign-in">
-            <div id="signIn-bg" className="w-full flex justify-end max-lg:justify-center bg-[url('./src/assets/images/winter2.jpg')] bg-cover-set">
+            <div id="signIn-bg" className="w-full flex justify-end max-lg:justify-center bg-cover-set" style={{backgroundImage: `url(${winter2})`}}>
                 <div id="row" className="flex w-2/5 max-lg:w-4/5 aspect-[7/6] m-10">
                     {/* Start input side */}
                     <motion.div id="input"
                     animate={{x: isClick ? '100%' : '0%', y:'0%'}} // isClick === true 的話 x 座標移動到 100%，否則移動到 0% // y 座標不變
                     transition={{ duration: 1.2, ease: "easeInOut" }} //移動時間 1.2 秒，使用 easeInOut 的動畫效果
-                    className={`bg-sky-500 border w-1/2 order-1 flex justify-center items-center px-7 py-5 bg-[url('./src/assets/images/kami.jpg')] bg-cover-set`}>
+                    className="bg-sky-500 border w-1/2 order-1 flex justify-center items-center px-7 py-5 bg-cover-set" style={{backgroundImage: `url(${kami})`}}>
                         <AnimatePresence mode="wait"> {/* 這個屬性會在動畫完成後再開始下一個動畫 */}
                             <motion.span
                             key={isClick}
@@ -39,7 +42,7 @@ function SignIn(){
                     <motion.div id="img" 
                     animate={{x: isClick ? '-100%' : '0%',y:'0%'} }
                     transition={{ duration: 1.2, ease: "easeInOut" }}
-                    className={`bg-green-500 border w-1/2 order-2 bg-[url('./src/assets/images/winter.jpg')] bg-cover-set`}></motion.div>
+                    className={`bg-green-500 border w-1/2 order-2 bg-cover-set`} style={{backgroundImage: `url(${winter})`}}></motion.div>
                     {/* End img side */}
 
                     {/* Start change btn */}
