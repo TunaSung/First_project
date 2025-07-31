6bd1034d881bce27d7afd97a5bbee6607d8f5470
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-07-31 16:24:23 +0800

    更改函式名稱

--- frontend/src/service/authService.js
+++ frontend/src/service/authService.js
@@ -10,28 +10,23 @@ export const saveToken = (token) => {
   
 // 清除 token、timestamp、header
 export const clearToken = () => {
-localStorage.removeItem("token");
-localStorage.removeItem("tokenSavedAt");
-delete api.defaults.headers.common["Authorization"];
+    localStorage.removeItem("token");
+    localStorage.removeItem("tokenSavedAt");
+    delete api.defaults.headers.common["Authorization"];
 };
 
 // 在其他 API 請求前呼叫，確保 header 裝載最新 token
 export const setAuthHeader = () => {
-const token = localStorage.getItem("token");
-if (token) {
-    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-}
+    const token = localStorage.getItem("token");
+    if (token) {
+        api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    }
 };
 
 // 登入
-export const login = async (email, password) => { // 後端需要什麼就跟前端要相應的變數
+export const signIn = async (email, password) => { // 後端需要什麼就跟前端要相應的變數
     try {
         const response = await api.post(`/auth/login`, { email, password }); 
-
-        if (response.data.token) {
-            saveToken(response.data.token);
-        }
-
         return response.data;
     } catch (error) {
         throw error.response?.data?.message || "Login failed";
@@ -39,16 +34,11 @@ export const login = async (email, password) => { // 後端需要什麼就跟前
 };
 
 // 註冊
-export const register = async (email, password) => {
+export const signUp = async (email, password) => {
     try {
         const response = await api.post(`/auth/register`, { email, password });
         return response.data;
     } catch (error) {
         throw error.response?.data?.message || "Registration failed";
     }
-};
-
-export const logout = () => {
-    localStorage.removeItem('token');
-    delete api.defaults.headers.common['Authorization']; // 刪除全域的 Authorization 標頭
 };
