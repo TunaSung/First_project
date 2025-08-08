c7da3426ea3dff91ee5efe7c5ea8ea7cd55962d4
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-08-08 18:21:44 +0800

    正確使用外鍵鍵名

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -8,7 +8,7 @@ Order.init(
     orderId: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     status: { type: DataTypes.STRING, defaultValue: "pending" },
     merchantTradeNo: { type: DataTypes.STRING, unique: true },
-    userId: {type: DataTypes.INTEGER, allowNull: false, references: {model: 'users', key: "id"}}
+    userId: {type: DataTypes.INTEGER, allowNull: false, references: {model: 'users', key: "userId"}}
     },
     {sequelize: sqlize, modelName:'order', tableName:'orders'}
 );
